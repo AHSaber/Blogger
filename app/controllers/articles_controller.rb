5ada9615aa3c25b5283af92a41ae1bf9a32ad7b2
Alex Saber <asaber@caltropcorp.com>
2015-07-18 18:12:48 -0700

    second commit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,42 @@
+include ArticlesHelper
+
 class ArticlesController < ApplicationController
+
 	def index
 		@articles = Article.all
 	end
+
 	def show
 		@article = Article.find(params[:id])
+	end
+
+	def new
+		@article = Article.new
+	end
+
+	def create
+		@article = Article.new(article_params)
+		@article.save
+		flash.notice = "The Article was posted!"
+		redirect_to article_path(@article)
+	end
+
+	def destroy
+		Article.find(params[:id]).destroy
+		flash.notice = "The Article was successfully deleted!"
+		redirect_to articles_path
+	end
+
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+	def update
+		@article = Article.find(params[:id])
+		@article.update(article_params)
+
+		flash.notice = "The Article: " + @article.title + "was successfully updated!"
+
+		redirect_to article_path(@article)
 	end
 end
